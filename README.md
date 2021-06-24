bd853a9f89467465ece7a8d804faf3b977e07efa
Cyrena RAMDANI <cramdani@42.student.fr>
2021-06-24 21:22:05 +0200

    cercle 3

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # 42_project
+
+# Cercle_1
+
+- __libft__
+
+
+# Cercle_2
+
+-  __Get_next_line__
+- __printf__
+
+# Cercle_3
+
+- __Push_Swap__
+- __Minitalk__
+
---
+++ printf/printf_srcs.mk
@@ -0,0 +1,9 @@
+SRCS		=	srcs/ft_printf.c \
+				srcs/r_format.c\
+				srcs/parsing.c\
+				srcs/convert_utils.c\
+				srcs/ft_display_char.c\
+				srcs/ft_display_int.c \
+				srcs/ft_display_uint.c \
+				srcs/ft_uint_utils.c \
+				srcs/ft_utils.c
